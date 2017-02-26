034f1922e8d80a96fb37b53624bef52ef4154fbf
Oleksiy Ivanenko <oleksiy.ivanenko@gmail.com>
2017-02-26 23:41:14 +0100

    Add get by name and cca3 index

--- countries.go
+++ countries.go
@@ -3,16 +3,22 @@ package main
 import (
   "fmt"
   "os"
+  "strings"
   "encoding/json"
   "net/http"
   "io/ioutil"
 )
 
+type CountryName struct {
+  Common string `json:"common"`
+  Official string `json:"official"`
+}
+
 type Country struct {
   Name struct {
     Common string `json:"common"`
     Official string `json:"official"`
-    // Native map[string] map[string] string
+    Native map[string] CountryName `json:"native"`
   } `json:"name"`
   Tld []string `json:"tld"`
 
@@ -28,6 +34,7 @@ type Country struct {
   Subregion string `json:"subregion"`
   Languages map[string] string `json:"languages"`
 
+  Translations map[string] CountryName `json:"translations"`
   Latlng []float32 `json:"latlng"`
   Denonym string `json:"denonym"`
   Landlocked bool `json:"landlocked"`
@@ -39,7 +46,10 @@ var (
   js []byte
   err error
 )
-var countriesMap map[string] []byte
+
+var cca2Countries map[string] []byte
+var cca3Countries map[string] []byte
+var nameCountries map[string] []byte
 
 
 func main() {
@@ -61,14 +71,21 @@ func main() {
     os.Exit(1)
   }
 
-  countriesMap = make(map[string] []byte)
+  cca2Countries = make(map[string] []byte)
+  cca3Countries = make(map[string] []byte)
+  nameCountries = make(map[string] []byte)
+
   for k := range countries {
-    countriesMap[countries[k].Cca2], _ = json.Marshal(countries[k])
+    marshaledCountry, _ := json.Marshal(countries[k])
+
+    cca2Countries[strings.ToLower(countries[k].Cca2)] = marshaledCountry
+    cca3Countries[strings.ToLower(countries[k].Cca3)] = marshaledCountry
+    nameCountries[strings.ToLower(countries[k].Name.Common)] = marshaledCountry
   }
 
   fmt.Printf("Serving\n")
-  http.HandleFunc("/api/countries", allCountries)
-  http.HandleFunc("/api/countries/", oneCountry)
+  http.HandleFunc("/api/v1/countries", allCountries)
+  http.HandleFunc("/api/v1/countries/", oneCountry)
   http.ListenAndServe(":8080", nil)
 }
 
@@ -78,7 +95,15 @@ func allCountries(w http.ResponseWriter, r *http.Request) {
 }
 
 func oneCountry(w http.ResponseWriter, r *http.Request) {
-  index := r.URL.Path[len("/api/countries/"):]
+  index := r.URL.Path[len("/api/v1/countries/"):]
+  index = strings.ToLower(index)
   w.Header().Set("Content-Type", "application/json")
-  w.Write(countriesMap[index])
+
+  if len(index) == 2 {
+    w.Write(cca2Countries[index])
+  } else if len(index) == 3 {
+    w.Write(cca3Countries[index])
+  } else {
+    w.Write(nameCountries[index])
+  }
 }
